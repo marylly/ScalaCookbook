5ae65983308f905157b3847a3759471d3c81baec
Marylly Araujo Silva <marylly.silva@agilitynetworks.com.br>
2018-08-13 21:21:45 -0300

    Finish Finding Patterns in Strings topic, chapter 1

--- 1.Strings/1.6 Finding Patterns in Strings.sh
+++ 1.Strings/1.6 Finding Patterns in Strings.sh
@@ -13,3 +13,20 @@ println(match1)
 val matches = numPattern.findAllIn(address).toArray
 
 matches.foreach(println)
+
+val address2 = "No address given"
+val match2 = numPattern.findFirstIn(address2)
+println(match2)
+
+val result = numPattern.findFirstIn(address2).getOrElse("no match")
+println(result)
+
+numPattern.findFirstIn(address).foreach{ e =>
+    println(e)
+}
+
+/*
+val match3 match {
+    case Some(s) => println(s"Found: $s")
+    case None =>
+}*/
